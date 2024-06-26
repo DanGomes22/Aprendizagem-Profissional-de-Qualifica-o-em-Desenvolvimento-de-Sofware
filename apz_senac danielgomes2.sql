c3065a962d5f002605e41fbdea707df8210f5f3b
DanGomes22 <dangomes2206@gmail.com>
2024-06-26 17:25:05 -0300

    Add files via upload

---
+++ apz_senac danielgomes2.sql
@@ -0,0 +1,4 @@
+insert into cliente(codigo, nome, endereco, cpf, telefone)
+values
+('9998887766', 'Joao da Silva', 'Rua da Saudade 1004, Fortaleza CE', '999888777666', '61999999999');
+select * from cliente
